7ceabf9b5de26f6dc68e2eb5a85639fc389d9aa7
Adam <adam@example.com>
2019-12-31 08:40:56 +0000

    add one test for SynonymStore::removeSynonyms

--- src/main/java/org/vorin/bestwords/loaders/SynonimNetParser.java
+++ src/main/java/org/vorin/bestwords/loaders/SynonimNetParser.java
@@ -4,18 +4,12 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.vorin.bestwords.util.Logger;
-import org.vorin.bestwords.util.Util;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import static java.lang.String.format;
-import static org.vorin.bestwords.util.Sources.LINGUEE_SOURCE;
 import static org.vorin.bestwords.util.Sources.SYNONIM_NET_SOURCE;
 
 /**
@@ -40,7 +34,7 @@ public class SynonimNetParser implements TranslationDataParser {
             translationPublisher.addMeaning(wordInfo.getParsedForeignWord(), row.text(), SYNONIM_NET_SOURCE);
             count++;
         }
-        LOG.info(format("added %s synonyms from [%s] for word [%s]", count, SYNONIM_NET_SOURCE, wordInfo.getParsedForeignWord()));
+//        LOG.info(format("added %s synonyms from [%s] for word [%s]", count, SYNONIM_NET_SOURCE, wordInfo.getParsedForeignWord()));
     }
 
     @Override
--- src/test/java/org/vorin/bestwords/loaders/SynonymStoreTest.java
+++ src/test/java/org/vorin/bestwords/loaders/SynonymStoreTest.java
@@ -3,10 +3,8 @@ package org.vorin.bestwords.loaders;
 import org.junit.Test;
 
 import java.util.Arrays;
-import java.util.Set;
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.doReturn;
 import static org.hamcrest.CoreMatchers.is;
 
 public class SynonymStoreTest {
@@ -27,6 +25,26 @@ public class SynonymStoreTest {
                 is(Arrays.asList("auto", "idiota", "dryndac")));
         assertThat(synonymStore.removeSynonyms(Arrays.asList("duren", "idiota", "auto", "dryndac")),
                    is(Arrays.asList("duren", "auto", "dryndac")));
+        assertThat(synonymStore.removeSynonyms(Arrays.asList("auto", "idiota", "telefonowac", "duren", "dryndac")),
+                is(Arrays.asList("auto", "duren", "dryndac")));
+    }
+
+    @Test
+    public void removeSynonyms2() {
+        // given
+        var synonymStore = new SynonymStore();
+        synonymStore.addMeaning("duren", "idiota", "");
+        synonymStore.addMeaning("dryndac", "telefonowac", "");
+
+        synonymStore.addMeaning("auto", "", "samochod");
+        synonymStore.addMeaning("idiota", "", "duren");
+        synonymStore.addMeaning("telefonowac", "", "dryndac");
+
+        // when & then
+        assertThat(synonymStore.removeSynonyms(Arrays.asList("auto", "idiota", "dryndac", "telefonowac")),
+                is(Arrays.asList("auto", "idiota", "dryndac")));
+        assertThat(synonymStore.removeSynonyms(Arrays.asList("duren", "idiota", "auto", "dryndac")),
+                is(Arrays.asList("duren", "auto", "dryndac")));
         assertThat(synonymStore.removeSynonyms(Arrays.asList("auto", "idiota", "telefonowac", "duren", "dryndac")),
                 is(Arrays.asList("auto", "duren", "dryndac")));
     }
